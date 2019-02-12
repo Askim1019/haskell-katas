94bc059ce830abb20503bdc24803ba87022b1129
Alex Kim <akim@futurefinance.com>
2019-02-12 15:38:10 -0600

    Ex 7 and 8

--- test/Ex07_FlowGuardsSpec.hs
+++ test/Ex07_FlowGuardsSpec.hs
@@ -8,23 +8,35 @@ main :: IO ()
 main = hspec spec
 
 -- BMI boundaries are 18.5, 25, 30
--- bmiTell :: (Ord a, Fractional a) => a -> String
--- max' :: (Ord a) => a -> a -> a
--- compare' :: (Ord a) => a -> a -> Ordering
+bmiTell :: (Ord a, Fractional a) => a -> String
+bmiTell x
+  | x <= 18.5 = "You're underweight, you emo, you!"
+  | x < 25 = "You're supposedly normal."
+  | x < 30 = "You're fat! Lose some weight!"
+  | otherwise = "You're a whale, congratulations!"
+
+max' :: (Ord a) => a -> a -> a
+max' x y
+  | x > y = x
+  | otherwise = y
+
+compare' :: (Ord a) => a -> a -> Ordering
+compare' x y
+  | x > y = GT
+  | x == y = EQ
+  | otherwise = LT
 
 spec :: Spec
 spec =
   describe "guards, guards!" $ do
     it "can be considered as a conditional, but easier to read" $ do
-      pending
-      -- bmiTell 15.2 `shouldBe` "You're underweight, you emo, you!"
-      -- bmiTell 23 `shouldBe` "You're supposedly normal."
-      -- bmiTell 27 `shouldBe` "You're fat! Lose some weight!"
-      -- bmiTell 31.4 `shouldBe` "You're a whale, congratulations!"
+      bmiTell 15.2 `shouldBe` "You're underweight, you emo, you!"
+      bmiTell 23 `shouldBe` "You're supposedly normal."
+      bmiTell 27 `shouldBe` "You're fat! Lose some weight!"
+      bmiTell 31.4 `shouldBe` "You're a whale, congratulations!"
     it "can calculate max from two values" $ do
-      pending
-      -- max' 1 3 `shouldBe` 3
+      max' 1 3 `shouldBe` 3
+      max' 3 1 `shouldBe` 3
     it "can compare two values" $ do
-      pending
-      -- compare' 2 2 `shouldBe` EQ
-      -- 2 `compare'` 3 `shouldBe` LT
+      compare' 2 2 `shouldBe` EQ
+      2 `compare'` 3 `shouldBe` LT
--- test/Ex08_FlowWhereSpec.hs
+++ test/Ex08_FlowWhereSpec.hs
@@ -6,27 +6,41 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec spec
-    -- | bmi <= skinny = "You're underweight, you emo, you!"
-    -- | bmi <= normal = "You're supposedly normal."
-    -- | bmi <= fat = "You're fat! Lose some weight!"
-    -- | otherwise   = "You're a whale, congratulations!"
 
 -- BMI boundaries are 18.5, 25.0 and 30.0
 -- Calculation logic: weight / height ^ 2
 -- bmiTell weight height
--- initials :: String -> String -> String
--- calcBmis :: Fractional t => [(t, t)] -> [t]
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal."
+    | bmi <= fat = "You're fat! Lose some weight!"
+    | otherwise   = "You're a whale, congratulations!"
+    where
+      skinny = 18.5
+      normal = 25.0
+      fat = 30.0
+      bmi :: Double
+      bmi = weight / height ^ 2
+
+initials :: String -> String -> String
+initials "" "" = ""
+initials (x:_) (y:_) = [x,y]
+initials (x:_) "" = [x]
+
+calcBmis :: Fractional t => [(t, t)] -> [t]
+calcBmis xs = [w / h ^ 2 | (w,h) <- xs]
+  -- where
+    -- calcBmis w h = w / h ^ 2
+
 
 spec :: Spec
 spec =
   describe "where - to DRY up logic" $ do
     it "can calculate BMI from values" $ do
-      pending
-      -- bmiTell 85 1.90 `shouldBe` "You're supposedly normal."
+      bmiTell 85 1.90 `shouldBe` "You're supposedly normal."
     it "can extract initials from a string" $ do
-      pending
-      -- initials "" "" `shouldBe` ""
-      -- initials "Attila" "Domokos" `shouldBe` "AD"
+      initials "" "" `shouldBe` ""
+      initials "Attila" "Domokos" `shouldBe` "AD"
     it "can be used in list comprehensions" $ do
-      pending
-      -- calcBmis [(85, 1.90)] `shouldBe` [23.545706371191137]
+      calcBmis [(85, 1.90)] `shouldBe` [23.545706371191137]
